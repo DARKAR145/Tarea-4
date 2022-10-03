c4be67630892a6ae058be0d7c669337724348697
LAPTOP-RICHARD\richi <richie-pb@htomail.com>
2022-10-03 14:16:41 -0500

    2

---
+++ Insertar informacion.sql
@@ -0,0 +1,54 @@
+use torneo_videojuego;
+
+insert Into videojuego Values ('1', 'Call of Duty', 'Treyarch', 'Activision');
+
+insert into torneo Values ('11', '1','Jueves 4 de Agosto', 'Domingo 7 de Agosto', 'Times Square', 'New York', 'Estados Unidos');
+
+insert into Equipo Values ('101', '4', 'Crimsix', 'Kismet', 'Hydra', 'Paulehx', 'NY', 'Subliners', 'Amarillo' );
+insert into Equipo Values ('102', '4', 'Cellium', 'Simp', 'Abezy', 'Arcitys', 'ATL', 'Faze', 'Rojo' );
+insert into Equipo Values ('103', '4', 'Accuracy', 'Mack', 'Sib', 'Pred', 'SEA', 'Surge', 'Verde oscuro' );
+insert into Equipo Values ('104', '4', 'Zero', 'Afro', 'Nastie', 'Gismo', 'LDN', 'Royal Ravens', 'Rojo oscuro' );
+insert into Equipo Values ('105', '4', 'Bance', 'Cammy', 'Cleanx', 'Insight', 'Tor', 'Ultra', 'Morado' );
+insert into Equipo Values ('106', '4', 'Shotzzy', 'Illey', 'Dashy', 'Scump', 'TX', 'Optic', 'Negro' );
+insert into Equipo Values ('107', '4', 'Methodz', 'Tjhaly', 'Vivid', 'Nero', 'Bos', 'Breach', 'Verde claro' );
+insert into Equipo Values ('108', '4', 'Kenny', 'Octane', 'Envoy', 'Drazah', 'LAt', 'Thieves', 'Rojo Sangre' );
+
+insert into jugador values ('110','Crimsix','27','Americana','101');
+insert into jugador values ('111','Kismet','19','Americana','101');
+insert into jugador values ('112','Hydra','21','Americana','101');
+insert into jugador values ('113','Paulehx','25','Americana','101');
+
+insert into jugador values ('114','Cellium','23','Americana','102');
+insert into jugador values ('115','Simp','30','Americana','102');
+insert into jugador values ('116','Abezy','28','Americana','102');
+insert into jugador values ('117','Arcitys','31','Americana','102');
+
+insert into jugador values ('118','Accuracy','22','Americana','103');
+insert into jugador values ('119','Mack','30','Americana','103');
+insert into jugador values ('120','Sib','23','Americana','103');
+insert into jugador values ('121','Pred','20','Americana','103');
+
+insert into jugador values ('122','Zero','30','Britanica','104');
+insert into jugador values ('123','Afro','21','Britanica','104');
+insert into jugador values ('124','Nastie','27','Britanica','104');
+insert into jugador values ('125','Gismo','28','Britanica','104');
+
+insert into jugador values ('126','Bance','21','Canadiense','105');
+insert into jugador values ('127','Cammy','25','Canadiense','105');
+insert into jugador values ('128','Cleanx','19','Canadiense','105');
+insert into jugador values ('129','Insight','24','Canadiense','105');
+
+insert into jugador values ('130','Shottzy','27','Americana','106');
+insert into jugador values ('131','Illey','30','Americana','106');
+insert into jugador values ('132','Dashy','27','Americana','106');
+insert into jugador values ('133','Scump','24','Americana','106');
+
+insert into jugador values ('134','Methodz','24','Americana','107');
+insert into jugador values ('135','Tjhaly','19','Americana','107');
+insert into jugador values ('136','Vivid','22','Americana','107');
+insert into jugador values ('137','Nero','27','Americana','107');
+
+insert into jugador values ('138','Kenny','23','Americana','108');
+insert into jugador values ('139','Octane','27','Americana','108');
+insert into jugador values ('140','Envoy','33','Americana','108');
+insert into jugador values ('141','Drazah','22','Americana','108');
